bb1cc3ed74c22bdc84f4a408f8b1d5c550695abf
Axel Magnuson <axelmagn@gmail.com>
2023-04-16 16:30:11 -0700

    event handling

--- src/bin/noise_viewer.rs
+++ src/bin/noise_viewer.rs
@@ -7,8 +7,8 @@
 
 use bevy::{
     prelude::{
-        default, App, Assets, Camera2dBundle, Commands, Handle, Image, Input, KeyCode, Query, Res,
-        ResMut, Resource,
+        default, App, Assets, Camera2dBundle, Commands, EventReader, EventWriter, Handle, Image,
+        Input, KeyCode, Query, Res, ResMut, Resource,
     },
     sprite::SpriteBundle,
     window::Window,
@@ -17,6 +17,7 @@ use bevy::{
 use clap::Parser;
 use image::{DynamicImage, RgbaImage};
 use indicatif::ProgressIterator;
+use log::info;
 use noise::{NoiseFn, ScalePoint};
 use palette::{
     encoding::{Linear, Srgb},
@@ -24,7 +25,8 @@ use palette::{
     Gradient, LinSrgb,
 };
 use proc_art::noise::NoiseSelector;
-use rand::{distributions::Uniform, thread_rng, Rng};
+use rand::{distributions::Uniform, thread_rng, Rng, SeedableRng};
+use rand_chacha::ChaChaRng;
 use tiny_skia::{
     Color as SkiaColor, FillRule, Paint, PathBuilder, Pixmap, PremultipliedColorU8,
     Transform as SkiaTransform,
@@ -33,16 +35,25 @@ use tiny_skia::{
 #[derive(Parser, Resource, Debug)]
 #[command(author, version, about, long_about=None)]
 struct Args {
-    /// random seed
+    /// initial random seed
     #[arg(long)]
     seed: Option<u64>,
 
+    /// type of random noise
     #[arg(long, value_enum, default_value_t = NoiseSelector::Perlin)]
     noise_type: NoiseSelector,
 
     /// noise scale
     #[arg(long, default_value_t = 4.)]
     noise_scale: f64,
+
+    /// window width
+    #[arg(long, default_value_t = 800.)]
+    width: f64,
+
+    /// window height
+    #[arg(long, default_value_t = 600.)]
+    height: f64,
 }
 
 impl Args {
@@ -57,24 +68,45 @@ impl Args {
         let noise_fn = ScalePoint::new(noise_fn).set_scale(scale);
         Box::new(noise_fn)
     }
+
+    fn get_seed(&self) -> u64 {
+        match self.seed {
+            Some(s) => s,
+            None => thread_rng().gen(),
+        }
+    }
 }
 
 #[derive(Resource, Default, Debug)]
 struct DisplayImage(Handle<Image>);
 
+/// Resource containing the current random seed.  This is different from the seed provided in Args, which is just the initial seed provided to the system.
+#[derive(Resource, Default, Debug)]
+struct RandomSeed(u64);
+
+enum ResourceUpdatedEvent {
+    Args,
+    RandomSeed,
+    WindowSize,
+}
+
 fn main() {
     let args = Args::parse();
+    let seed = RandomSeed(args.get_seed());
 
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_event::<ResourceUpdatedEvent>()
         .insert_resource(args)
+        .insert_resource(seed)
         .init_resource::<DisplayImage>()
-        .add_startup_system(setup)
+        .add_startup_system(bevy_setup)
+        .add_system(handle_input)
         .add_system(update_display)
         .run();
 }
 
-fn setup(
+fn bevy_setup(
     mut commands: Commands,
     mut images: ResMut<Assets<Image>>,
     mut display_img: ResMut<DisplayImage>,
@@ -103,41 +135,79 @@ fn setup(
     });
 }
 
+fn handle_input(
+    keys: Res<Input<KeyCode>>,
+    mut args: ResMut<Args>,
+    mut seed: ResMut<RandomSeed>,
+    mut ev_updated: EventWriter<ResourceUpdatedEvent>,
+) {
+    // Spacebar - generate new random seed
+    if keys.just_pressed(KeyCode::Space) {
+        *seed = RandomSeed(thread_rng().gen());
+        info!("random seed: {}", seed.0);
+        ev_updated.send(ResourceUpdatedEvent::RandomSeed);
+    }
+
+    if keys.just_pressed(KeyCode::Tab) {
+        if keys.any_pressed([KeyCode::LShift, KeyCode::RShift]) {
+            args.noise_type = args.noise_type.get_prev();
+        } else {
+            args.noise_type = args.noise_type.get_next();
+        }
+        info!("noise type: {:?}", args.noise_type);
+        ev_updated.send(ResourceUpdatedEvent::Args);
+    }
+}
+
 /// update the display image when spacebar is pressed
 fn update_display(
-    keys: Res<Input<KeyCode>>,
-    mut images: ResMut<Assets<Image>>,
+    mut ev_updated: EventReader<ResourceUpdatedEvent>,
     display_img: Res<DisplayImage>,
     args: Res<Args>,
+    seed: Res<RandomSeed>,
     window: Query<&Window>,
+    mut images: ResMut<Assets<Image>>,
 ) {
-    if keys.just_pressed(KeyCode::Space) {
-        println!("updating display...");
-        // set up random generation of colors
-        let mut rng = thread_rng();
-        let window_w = window.single().resolution.width() as u32;
-        let window_h = window.single().resolution.height() as u32;
-        let noise_fn = args.get_scaled_noise(rng.gen(), window_w, window_h);
-
-        // TODO: read from palette files
-        let colors: Vec<_> = (0..5)
-            .map(|i| {
-                let range = Uniform::new(0., 1. / 5. * (i + 1) as f64);
-                let r = rng.sample(range);
-                let g = rng.sample(range);
-                let b = rng.sample(range);
-                LinSrgb::new(r, g, b)
-            })
-            .collect();
-
-        // let pixmap = paint_noise(window_w, window_h, &mut rng);
-        let pixmap = paint_noise(&noise_fn, &colors, window_w, window_h);
-
-        let bvy_img = images.get_mut(&display_img.0).unwrap();
-        let rgba = RgbaImage::from_raw(window_w, window_h, pixmap.data().into()).unwrap();
-        let dyn_img = DynamicImage::ImageRgba8(rgba);
-        *bvy_img = Image::from_dynamic(dyn_img, false);
+    // check if we care about anything that refreshed
+    let mut should_refresh = false;
+    for ev in ev_updated.iter() {
+        match ev {
+            ResourceUpdatedEvent::Args | ResourceUpdatedEvent::RandomSeed => {
+                should_refresh = true;
+            }
+        }
     }
+    ev_updated.clear();
+    if !should_refresh {
+        return;
+    }
+
+    info!("updating display...");
+
+    // set up random noise
+    let mut rng = ChaChaRng::seed_from_u64(seed.0);
+    let window_w = window.single().resolution.width() as u32;
+    let window_h = window.single().resolution.height() as u32;
+    let noise_fn = args.get_scaled_noise(rng.gen(), window_w, window_h);
+
+    // TODO: read from palette files
+    let colors: Vec<_> = (0..5)
+        .map(|i| {
+            let range = Uniform::new(0., 1. / 5. * (i + 1) as f64);
+            let r = rng.sample(range);
+            let g = rng.sample(range);
+            let b = rng.sample(range);
+            LinSrgb::new(r, g, b)
+        })
+        .collect();
+
+    // let pixmap = paint_noise(window_w, window_h, &mut rng);
+    let pixmap = paint_noise(&noise_fn, &colors, window_w, window_h);
+
+    let bvy_img = images.get_mut(&display_img.0).unwrap();
+    let rgba = RgbaImage::from_raw(window_w, window_h, pixmap.data().into()).unwrap();
+    let dyn_img = DynamicImage::ImageRgba8(rgba);
+    *bvy_img = Image::from_dynamic(dyn_img, false);
 }
 
 fn paint_circle_flag<R: Rng>(width: u32, height: u32, rng: &mut R) -> Pixmap {
--- src/noise.rs
+++ src/noise.rs
@@ -1,8 +1,13 @@
 use clap::ValueEnum;
 use noise::{Fbm, NoiseFn, Perlin, Simplex};
 
+/// TODO: deprecate this in favor of configured values
+
+/// NOTE: update this whenever number of selectors changes
+pub const NOISE_SELECTORS_LEN: isize = 3;
+
 /// A flat enum for selecting noise functions as a CLI option or config variable.
-#[derive(Debug, Clone, ValueEnum)]
+#[derive(Debug, Clone, Copy, ValueEnum)]
 pub enum NoiseSelector {
     Simplex,
     Perlin,
@@ -16,5 +21,47 @@ impl NoiseSelector {
             Self::Perlin => Box::new(Perlin::new(seed)),
             Self::FbmPerlin => Box::new(Fbm::<Perlin>::new(seed)),
         }
+    }
+
+    pub fn get_next(&self) -> Self {
+        Self::from((*self as isize + 1) % NOISE_SELECTORS_LEN)
+    }
+
+    pub fn get_prev(&self) -> Self {
+        Self::from((*self as isize + NOISE_SELECTORS_LEN - 1) % NOISE_SELECTORS_LEN)
+    }
+}
+
+impl From<isize> for NoiseSelector {
+    fn from(idx: isize) -> Self {
+        match idx {
+            0 => Self::Simplex,
+            1 => Self::Perlin,
+            2 => Self::FbmPerlin,
+            _ => Self::default(),
+        }
+    }
+}
+
+impl Default for NoiseSelector {
+    fn default() -> Self {
+        Self::Perlin
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::noise::{NoiseSelector, NOISE_SELECTORS_LEN};
+
+    #[test]
+    fn test_noise_selector_from_idx() {
+        for i in 0..NOISE_SELECTORS_LEN {
+            assert_eq!(i, NoiseSelector::from(i) as isize)
+        }
+        // for larger numbers, should be equal to default
+        assert_eq!(
+            NoiseSelector::default() as isize,
+            NoiseSelector::from(255) as isize
+        );
     }
 }
